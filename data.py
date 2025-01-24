549fcbcb6bb1c49155609073a62e38088474164f
milliono <aimiliosdraginis@gmail.com>
2025-01-24 12:50:38 +0200

    log actions

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
+import helpers
+
 class Vehicle:
     def __init__(
         self, license, spot=None, driver_name=None, is_sub=False, datetime=None
@@ -29,6 +31,14 @@ class ParkingSpace:
         if reg is not False:
             self.subscription[reg]["occupied"] = True
             self.subscription[reg]["vehicle"].datetime = datetime
+            helpers.log_csv(
+                self.subscription[reg]["vehicle"].license,
+                "Monthly",
+                "Parked",
+                self.subscription[reg]["vehicle"].spot,
+                self.subscription[reg]["vehicle"].datetime,
+                f"${0.0}",
+            )
         else:
             # Park a car in the first available spot
             vec = Vehicle(license)
@@ -37,7 +47,15 @@ class ParkingSpace:
                     self.hourly[index] = vec
                     vec.spot = index
                     vec.datetime = datetime
-                    return  # Stop once the car is parked
+                    helpers.log_csv(
+                        vec.license,
+                        "Hourly",
+                        "Parked",
+                        vec.spot,
+                        vec.datetime,
+                        f"${0.0}",
+                    )
+                    return
             print("No available spots.")
 
     def find_registered(self, license):
@@ -52,6 +70,14 @@ class ParkingSpace:
             self.subscription[vehicle.spot]["vehicle"] = vehicle
             date_key = vehicle.datetime.date().isoformat()
             self.profit[date_key] = self.profit.get(date_key, 0) + 50 * duration
+            helpers.log_csv(
+                vehicle.license,
+                "Monthly",
+                "Rented",
+                vehicle.spot,
+                vehicle.datetime,
+                f"${self.profit[date_key]:.2f}",
+            )
             return True
         return False
 
@@ -60,12 +86,28 @@ class ParkingSpace:
         if spot_type == "subscription":
             time = datetime - self.subscription[spot]["vehicle"].datetime
             self.subscription[spot]["occupied"] = False
+            helpers.log_csv(
+                self.subscription[spot]["vehicle"].license,
+                "Monthly",
+                "Pulled-out",
+                self.subscription[spot]["vehicle"].spot,
+                self.subscription[spot]["vehicle"].datetime,
+                f"${0.0}",
+            )
         elif spot_type == "hourly":
             time = datetime - self.hourly[spot].datetime
-            self.hourly[spot] = False
             date_key = datetime.date().isoformat()
             current_profit = self.calc_profit(time.total_seconds())
             self.profit[date_key] = self.profit.get(date_key, 0) + current_profit
+            helpers.log_csv(
+                self.hourly[spot].license,
+                "Hourly",
+                "Pulled-Out",
+                self.hourly[spot].spot,
+                self.hourly[spot].datetime,
+                f"${self.profit[date_key]:.2f}",
+            )
+            self.hourly[spot] = False
 
     def calc_profit(self, seconds, price=2):
         return (seconds / 3600) * price
--- helpers.py
+++ helpers.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 def save_parking_to_csv(
@@ -11,3 +12,22 @@ def save_parking_to_csv(
 
     df_hourly.to_csv(hourly_filename, index=False)
     df_subscription.to_csv(subscription_filename, index=False)
+
+
+def initialize_csv(file_path="actions.csv"):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    headers = ["license", "type", "action", "spot", "timestamp", "pay"]
+    df = pd.DataFrame(columns=headers)
+    df.to_csv(file_path, mode="w", header=True, index=False)
+
+
+def log_csv(license, type, action, spot, timestamp, pay):
+    new_data = pd.DataFrame(
+        [[license, type,  action, spot, timestamp, pay]],
+        columns=["license", "type", "action", "spot", "timestamp", "pay"],
+    )
+    new_data.to_csv("actions.csv", mode="a", header=False, index=False)
+
+
+initialize_csv()
